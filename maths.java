60ce27d2c62403187985112f2ae42094b991a33c
bioinformaticsvideos <131031539+bioinformaticsvideos@users.noreply.github.com>
2023-05-06 05:17:59 +0530

    Add files via upload
    basic program before making the calculator app

---
+++ maths.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+public class maths {
+    public static Scanner sc;
+   public static  void  main(String[] args) {
+
+
+            int a, b, sum , sub, div, mul;
+
+          sc= new Scanner(System.in);
+
+            System.out.println("Enter 1st number: ");
+            a = sc.nextInt();
+            System.out.println("Enter 2nd number: ");
+             b = sc.nextInt();
+             sum = a+b;
+              sub= a-b;
+              mul = a*b;
+              div = a/b;
+              System.out.println("\n sum of two nos is "+sum);
+       System.out.println("\n subtraction of two nos is "+sub);
+       System.out.println("\n quotient of two nos is "+div);
+       System.out.println("\n product of two nos is "+mul);
+
+        }
+
+
+        }
+
